e4d992970f5ac4521c3ad60c9e89768474ed7a80
Joe Legg <joelegg3@gmail.com>
2019-05-25 16:24:00 +0100

    Parse when statment.

--- src/parser.h
+++ src/parser.h
@@ -5,15 +5,17 @@
 #define TOK_CONNECTION                       5
 #define TOK_SEMICOLON                        6
 #define TOK_WIRE                             7
-#define TOK_MODULE                           8
+#define TOK_WHEN                             8
 #define TOK_END                              9
-#define TOK_COMMA                           10
-#define TOK_INPUT                           11
-#define TOK_OUTPUT                          12
-#define TOK_LBRACKET                        13
-#define TOK_RBRACKET                        14
-#define TOK_TRUE                            15
-#define TOK_FALSE                           16
-#define TOK_IDENT                           17
+#define TOK_ELSE                            10
+#define TOK_MODULE                          11
+#define TOK_COMMA                           12
+#define TOK_INPUT                           13
+#define TOK_OUTPUT                          14
+#define TOK_LBRACKET                        15
+#define TOK_RBRACKET                        16
+#define TOK_TRUE                            17
+#define TOK_FALSE                           18
+#define TOK_IDENT                           19
 
     void parse(const char *filename);
