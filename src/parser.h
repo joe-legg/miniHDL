58a2dd67c387e24da7e80816eb0829d29433f953
Joe Legg <joelegg3@gmail.com>
2019-05-25 16:06:20 +0100

    Wire definition parsing done.

--- src/parser.h
+++ src/parser.h
@@ -2,17 +2,18 @@
 #define TOK_XOR                              2
 #define TOK_OR                               3
 #define TOK_NOT                              4
-#define TOK_SEMICOLON                        5
-#define TOK_CONNECTION                       6
-#define TOK_MODULE                           7
-#define TOK_END                              8
-#define TOK_COMMA                            9
-#define TOK_INPUT                           10
-#define TOK_OUTPUT                          11
-#define TOK_LBRACKET                        12
-#define TOK_RBRACKET                        13
-#define TOK_TRUE                            14
-#define TOK_FALSE                           15
-#define TOK_IDENT                           16
+#define TOK_CONNECTION                       5
+#define TOK_SEMICOLON                        6
+#define TOK_WIRE                             7
+#define TOK_MODULE                           8
+#define TOK_END                              9
+#define TOK_COMMA                           10
+#define TOK_INPUT                           11
+#define TOK_OUTPUT                          12
+#define TOK_LBRACKET                        13
+#define TOK_RBRACKET                        14
+#define TOK_TRUE                            15
+#define TOK_FALSE                           16
+#define TOK_IDENT                           17
 
     void parse(const char *filename);
